0306b4f1acf7f30d44918f4cc1af2b786b7623eb
ellakummer <ella.kummer@etu.unige.ch>
2020-06-16 16:27:28 +0200

    merge all data for each station

--- src/mergeData.py
+++ src/mergeData.py
@@ -1,227 +1,131 @@
 import csv
 import numpy as np
-# for random forest
 import sklearn
-from sklearn.datasets import make_regression
-from numpy import mean
-from numpy import std
-from sklearn.datasets import make_regression
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import RepeatedKFold
-from sklearn.ensemble import RandomForestRegressor
-# for gradient boosting
-from sklearn.metrics import mean_squared_error
-from sklearn.datasets import make_friedman1
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import confusion_matrix, classification_report
-
-from sklearn.model_selection import GridSearchCV
-# for SVM with cross validation
-from sklearn.model_selection import train_test_split
-from sklearn import datasets
-from sklearn import svm
-from sklearn.model_selection import cross_val_score
-from sklearn import preprocessing
-from sklearn import utils
-# for GAM (pip3 install pygam)
-from pygam import GAM, s, f
-from pygam import PoissonGAM
-from pygam import LogisticGAM
-from pygam import LinearGAM
 import pandas as pd
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import log_loss
-# for Multiclass Neural Network
-'''
-from keras.callbacks import ModelCheckpoint
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
-import seaborn as sb
-import warnings
-warnings.filterwarnings('ignore')
-warnings.filterwarnings('ignore', category=DeprecationWarning)
-from xgboost import XGBRegressor
-'''
-# tests
-from sklearn.datasets import load_boston
-from sklearn.model_selection import train_test_split
-# plots
-import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
-
-
-# https://medium.com/mongolian-data-stories/ulaanbaatar-air-pollution-part-1-35e17c83f70b (ici plus discret mdr)
-# https://medium.com/mongolian-data-stories/part-3-the-model-b2fb9a25a07c
-# https://github.com/robertritz/Ulaanbaatar-PM2.5-Prediction/blob/master/Classification%20Model/Ulaanbaatar%20PM2.5%20Prediction%20-%20Classification.ipynb
-
-# If the wind speed is less than 0.5m/s (nearly no wind), the value of the wind direction is 999017.
-# -> put to 0
 
 # ALSO :
 # see how to VISUALIZE the datas
 # e.g. plot PMs per month ( overall visualisation, avoir une idée du truc - > dans intro rapport ? )
 # e.g. afficher PMs en fonction de chaque autre donnée qu'on a (un plot par donnée externe)
 
-# MODELS :
-# GradientBoostingRegressor, svm.SVC, RandomForestRegressor, LogisticGAM
-
-days = 2
+print("------------------------------ LOAD DATA  ------------------------------")
 
 # ------- LOAD array London_historical_aqi_forecast_stations_20180331 ------
 
 print(" -------------------- LOAD London_historical_aqi_forecast_stations_20180331")
-aqi_forecast = pd.read_csv('../final_project_data/London_historical_aqi_forecast_stations_20180331.csv')
 
+aqi_forecast = pd.read_csv('../final_project_data/London_historical_aqi_forecast_stations_20180331.csv')
 aqi_forecast = aqi_forecast.interpolate(method ='polynomial', order = 2, limit_direction ='forward')
 aqi_forecast = aqi_forecast.fillna(aqi_forecast.mean())
-
+'''
 print("test shape aqi_forecast: ")
 print(aqi_forecast.shape)
-
 C_mat_aqi_forecast = aqi_forecast.corr()
+print("correlation matrix : ")
 print(C_mat_aqi_forecast)
+print("Data aqi_forecast : ")
 print(aqi_forecast)
-'''
-x = aqi_forecast['MeasurementDateGMT']
-y = aqi_forecast['PM2.5 (ug/m3)']
-x = x[141602:141612]
-y = y[141602:141612]
-plt.plot(x,y)
-plt.xlabel('MeasurementDateGMT')
-plt.ylabel('PM2.5 Level')
-plt.title('PM2.5  from 2018/3/28 14:00 to 2018/3/29 0:00 , before interpolation')
-plt.show()
-'''
-
-#aqi_forecast = aqi_forecast.to_numpy()
-#print(aqi_forecast)
-#print(aqi_forecast.shape)
-'''
-x = aqi_forecast['MeasurementDateGMT']
-y = aqi_forecast['PM2.5 (ug/m3)']
-x = x[141602:141612]
-y = y[141602:141612]
-plt.plot(x,y)
-plt.xlabel('MeasurementDateGMT')
-plt.ylabel('PM2.5 Level')
-plt.title('PM2.5  from 2018/3/28 14:00 to 2018/3/29 0:00 , after interpolation')
-plt.show()
 '''
 print("")
 # ------- LOAD London_historical_aqi_other_stations_20180331 ------
 print(" -------------------- LOAD London_historical_aqi_other_stations_20180331")
+
 aqi_other = pd.read_csv('../final_project_data/London_historical_aqi_other_stations_20180331.csv')
-'''
-print("aqi_other lines : ")
-print(aqi_other.iloc[[3]])
-print("test shape aqi_other: ")
-print(aqi_other.shape)
-print(aqi_forecast.dtypes)
-print(aqi_other.dtypes)
-'''
 aqi_other = aqi_other.interpolate(method ='polynomial', order = 2, limit_direction ='forward')
 aqi_other = aqi_other.fillna(aqi_other.mean())
-
+'''
+print("correlation matrix : ")
 C_mat_aqi_other = aqi_other.corr()
 print(C_mat_aqi_other)
+print("Data aqi_other : ")
 print(aqi_other)
 '''
 print("")
-aqi_other = aqi_other.to_numpy()
-print("7th and 9th elements : ")
-print(aqi_other[7])
-print(aqi_other[9])
-print("test sum :")
-print(aqi_other[7][3] + aqi_other[9][3])
-'''
-
-print("")
 #------- LOAD London_AirQuality_Stations ------
 print("-------------------- LOAD London_AirQuality_Stations")
-airQuality_stations = pd.read_csv('../final_project_data/London_AirQuality_Stations.csv')
 
+airQuality_stations = pd.read_csv('../final_project_data/London_AirQuality_Stations.csv')
+'''
 print("test shape airQuality_stations: ")
 print(airQuality_stations.shape)
-
+print("Data airQuality_stations : ")
 print(airQuality_stations)
-'''
-print("")
-airQuality_stations = airQuality_stations.to_numpy()
-print("7th  elements : ")
-print(airQuality_stations[7])
 '''
 print("")
 # ------- LOAD London_grid_weather_station ------
 print(" -------------------- LOAD London_grid_weather_station")
-grid_stations = pd.read_csv('../final_project_data/London_grid_weather_station.csv')
 
+grid_stations = pd.read_csv('../final_project_data/London_grid_weather_station.csv')
+'''
 print("test shape grid_stations: ")
 print(grid_stations.shape)
-
+print("Data grid_stations : ")
 print(grid_stations)
-
+'''
 print("")
 # ------- LOAD London_historical_meo_grid ------
 print(" -------------------- LOAD London_historical_meo_grid")
+
 meo_grid = pd.read_csv('../../London_historical_meo_grid.csv')
 # TAKES A LOT OF TIME :
 #meo_grid = meo_grid.interpolate(method ='polynomial', order = 2, limit_direction ='forward')
 #meo_grid = meo_grid.fillna(meo_grid.mean())
-
+'''
 print("test shape meo_grid: ")
 print(meo_grid.shape)
-
+print("Correlation matrix : ")
 C_mat_meo_grid = meo_grid.corr()
 print(C_mat_meo_grid)
+print("Data meo_grid : ")
 print(meo_grid)
-
+'''
+print("")
 print("-------------------------- MERGE DATAS  --------------------------")
-# so we have : aqi_forecast, aqi_other, airQuality_stations, grid_stations, meo_grid
-# TRY TAKE FOR ONE STATION : BL0
-# BASIC INFOS (IDs)
+for station in airQuality_stations['id']:
+    print("Station : ")
+    print(station)
 
-# -- ICI ON ITERER SUR LES IDs :
-# bl0_airQuality_stations ->  id YYY :
-bl0_airQuality_station = airQuality_stations.loc[airQuality_stations['id']=='BL0',:]
-print("bl0_airQuality_station id: ")
-print(bl0_airQuality_station)
+    station_airQuality_station = airQuality_stations.loc[airQuality_stations['id']==station,:]
 
-# ON CHERCHERA DANS FORECAST ET OTHER PUIS CONCAT EN HAUTEUR
-#bl0_aqi_forecast <- id YYY
-bl0_aqi_forecast = aqi_forecast.loc[aqi_forecast['station_id']=='BL0',:]
-print("bl0_aqi_forecast : ")
-print(bl0_aqi_forecast)
-#print(bl0_aqi_forecast.iloc[[3]])
+    #station_aqi_forecast <- id YYY
+    station_aqi_forecast = aqi_forecast.loc[aqi_forecast['station_id']==station,:]
+    print("station_aqi_forecast : ")
+    print(station_aqi_forecast)
 
-#bl0_grid_stations : id YYY --> id london_grid_XX
-longitude_bl0 = float(f"{bl0_airQuality_station['Longitude'].values[0]:.1f}")
-latitude_bl0 = float(f"{bl0_airQuality_station['Latitude'].values[0]:.1f}")
-print("longitude bl0 : ")
-print(longitude_bl0)
-print("latitude bl0 : ")
-print(latitude_bl0)
-#print(type(longitude_bl0))
-#print(type(latitude_bl0))
+    station_aqi_other = aqi_other.loc[aqi_other['station_id']==station,:]
+    print("station_aqi_other : ")
+    print(station_aqi_other)
 
-bl0_grid_station = grid_stations.loc[grid_stations['longitude']==longitude_bl0,:].loc[grid_stations['latitude']==latitude_bl0,:]['stationName'].values[0]
-print("bl0_grid_station id : ")
-print(bl0_grid_station)
+    #station_grid_stations : id YYY --> id london_grid_XX (w/ longitude + latitude)
+    longitude_station = float(f"{station_airQuality_station['Longitude'].values[0]:.1f}")
+    latitude_station = float(f"{station_airQuality_station['Latitude'].values[0]:.1f}")
+    print("longitude station : ")
+    print(longitude_station)
+    print("latitude station : ")
+    print(latitude_station)
 
+    station_grid_station = grid_stations.loc[grid_stations['longitude']==longitude_station,:].loc[grid_stations['latitude']==latitude_station,:]['stationName'].values[0]
+    print("station_grid_station id : ")
+    print(station_grid_station)
 
-#bl0_meo_grid <- id london_grid_XX
-bl0_meo_grid = meo_grid.loc[meo_grid['stationName']==bl0_grid_station,:]
-print("bl0_meo_grid: ")
-print(bl0_meo_grid)
+    #station_meo_grid <- id london_grid_XX
+    station_meo_grid = meo_grid.loc[meo_grid['stationName']==station_grid_station,:]
+    print("station_meo_grid: ")
+    print(station_meo_grid)
 
-print("MERGE")
-'''
-print(bl0_aqi_forecast.columns)
-print(bl0_meo_grid.columns)
-'''
-bl0_merge =pd.merge(left=bl0_aqi_forecast, right=bl0_meo_grid, left_on='utc_time', right_on='utc_time')
+    print("MERGE")
+    '''
+    print(station_aqi_forecast.columns)
+    print(station_aqi_other.columns
+    print(station_meo_grid.columns)
+    '''
+    if station_aqi_forecast.empty :
+        station_merge =pd.merge(left=station_aqi_other, right=station_meo_grid, left_on='utc_time', right_on='utc_time')
+    else :
+        station_merge =pd.merge(left=station_aqi_forecast, right=station_meo_grid, left_on='utc_time', right_on='utc_time')
 
-print(bl0_merge)
-print(bl0_merge.columns)
+    print(station_merge)
+    print(station_merge.columns)
 
-# SAVE csv :
-bl0_merge.to_csv(r'../final_project_data/merge/BL0.csv', index = False)
+    # SAVE csv :
+    station_merge.to_csv(r'../final_project_data/merge/'+station+'.csv', index = False)
